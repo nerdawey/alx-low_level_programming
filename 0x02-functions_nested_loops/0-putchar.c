7c44637fbcead6ec5255703927275089edd94d56
nerdawey <nerdawy@icloud.com>
2023-02-21 16:16:46 -0800

    asd

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
-
 /**
- * print_alphabet - Make the alphabet
- * Return: void
+ * main - Entry point
+ * Return: Always 0
  */
 
-void print_alphabet(void)
+int main(void)
 
 {
 
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-	_putchar(c);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-}
+	return (0);
 
+}
